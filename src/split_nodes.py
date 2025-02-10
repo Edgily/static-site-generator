8482e2c367a5dc2852d71ff87204e3c1d0eef685
Edgily <endelean0a@outlook.com>
2025-02-10 09:36:27 +0000

    finalized split_nodes_image and its tests

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,43 +1,40 @@
 from textnode import TextNode, TextType
 from extract_markdown_urls import extract_markdown_links, extract_markdown_images
 
+
 def split_text_link(text):
     extracted_links = extract_markdown_links(text)
 
     if len(extracted_links) == 0:
-        if text == '':
+        if text == "":
             return []
         return [TextNode(text, TextType.TEXT)]
-    
+
     link_text, url = extracted_links[0]
     parts = text.split(f"[{link_text}]({url})", 1)
     result = []
 
-    if parts[0] != '':
+    if parts[0] != "":
         result.append(TextNode(parts[0], TextType.TEXT))
     result.append(TextNode(link_text, TextType.LINK, url))
     result.extend(split_text_link(parts[1]))
 
     return result
 
+
 def split_nodes_link(nodes):
     if not isinstance(nodes, list):
         raise ValueError("The provided input is not a list.")
     new_nodes = []
     for node in nodes:
-        if not node.text:
-            raise ValueError("The provided node has no text.")
-        result_nodes = split_text_link(node.text)
-        new_nodes.extend(result_nodes)
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            if not node.text:
+                raise ValueError("The provided node has no text.")
+            result_nodes = split_text_link(node.text)
+            new_nodes.extend(result_nodes)
     return new_nodes
-
-
-
-
-
-
-
-
 
 
 def split_text_image(text):
@@ -65,14 +62,11 @@ def split_nodes_image(nodes):
         raise ValueError("The provided input is not a list.")
     new_nodes = []
     for node in nodes:
-        result_nodes = split_text_image(node.text)
-        new_nodes.extend(result_nodes)
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            if not node.text:
+                raise ValueError("The provided node has no text.")
+            result_nodes = split_text_image(node.text)
+            new_nodes.extend(result_nodes)
     return new_nodes
-
-
-node = TextNode(
-    "Start ![img1](url1) middle ![img2](url2) end ![img3](url3)",
-    TextType.TEXT
-)
-new_nodes = split_nodes_image([node])
-print(new_nodes)
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -21,13 +21,19 @@ class TestSplitNodesLink(unittest.TestCase):
             split_nodes_link(nodes)
         self.assertEqual(str(context.exception), "The provided node has no text.")
 
-    def test_no_links(self):
+    def test_wrong_node_type(self):
+        nodes = [TextNode("Text", TextType.CODE)]
+        new_nodes = split_nodes_link(nodes)
+        self.assertEqual(new_nodes[0].text, "Text")
+        self.assertEqual(new_nodes[0].text_type, TextType.CODE)
+
+    def test_no_link(self):
         nodes = [TextNode("Just plain text", TextType.TEXT)]
         new_nodes = split_nodes_link(nodes)
         self.assertEqual(len(new_nodes), 1)
         self.assertEqual(new_nodes[0].text, "Just plain text")
         self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
-    
+
     def test_single_link(self):
         node = TextNode(
             "This is text with a link [to boot dev](https://www.boot.dev)",
@@ -75,24 +81,120 @@ class TestSplitNodesLink(unittest.TestCase):
         self.assertEqual(new_nodes[1].text_type, TextType.LINK)
         self.assertEqual(new_nodes[1].url, "https://youtube.com")
 
+    def test_mixed_node_types(self):
+        nodes = [
+            TextNode("Code block", TextType.CODE),
+            TextNode("Normal text with ![alt](url)", TextType.TEXT),
+            TextNode("Link text", TextType.LINK, "url"),
+        ]
+        new_nodes = split_nodes_image(nodes)
 
+        self.assertEqual(new_nodes[0].text, "Code block")
+        self.assertEqual(new_nodes[0].text_type, TextType.CODE)
 
+        self.assertEqual(new_nodes[1].text, "Normal text with ")
+        self.assertEqual(new_nodes[1].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[2].text, "alt")
+        self.assertEqual(new_nodes[2].text_type, TextType.IMAGE)
 
-
-
-
-
+        self.assertEqual(new_nodes[3].text, "Link text")
+        self.assertEqual(new_nodes[3].text_type, TextType.LINK)
 
 
 class TestSplitNodesImage(unittest.TestCase):
     def test_not_list(self):
         with self.assertRaises(ValueError) as context:
-            split_nodes_link("not a list")
+            split_nodes_image("not a list")
         self.assertEqual(str(context.exception), "The provided input is not a list.")
 
-    def test_split_nodes_link_empty_list(self):
+    def test_empty_list(self):
         nodes = []
-        new_nodes = split_nodes_link(nodes)
+        new_nodes = split_nodes_image(nodes)
         self.assertEqual(len(new_nodes), 0)
 
-    
+    def test_no_text(self):
+        nodes = [TextNode("", TextType.TEXT)]
+        with self.assertRaises(ValueError) as context:
+            split_nodes_image(nodes)
+        self.assertEqual(str(context.exception), "The provided node has no text.")
+
+    def test_wrong_node_type(self):
+        nodes = [TextNode("Text", TextType.CODE)]
+        new_nodes = split_nodes_image(nodes)
+        self.assertEqual(new_nodes[0].text, "Text")
+        self.assertEqual(new_nodes[0].text_type, TextType.CODE)
+
+    def test_no_image(self):
+        nodes = [TextNode("Just plain text", TextType.TEXT)]
+        new_nodes = split_nodes_image(nodes)
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0].text, "Just plain text")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+
+    def test_single_image(self):
+        node = TextNode(
+            "This is text with an image ![img1](https://www.img1.com)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with an image ", TextType.TEXT),
+                TextNode("img1", TextType.IMAGE, "https://www.img1.com"),
+            ],
+        )
+
+    def test_multiple_images(self):
+        nodes = [
+            TextNode(
+                "Img1 ![img1](https://img1.com) img2 ![img2](https://img2.com)",
+                TextType.TEXT,
+            )
+        ]
+        new_nodes = split_nodes_image(nodes)
+        self.assertEqual(len(new_nodes), 4)
+        self.assertEqual(new_nodes[0].text, "Img1 ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "img1")
+        self.assertEqual(new_nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[1].url, "https://img1.com")
+        self.assertEqual(new_nodes[2].text, " img2 ")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[3].text, "img2")
+        self.assertEqual(new_nodes[3].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[3].url, "https://img2.com")
+
+    def test_consecutive_images(self):
+        nodes = [
+            TextNode(
+                "![img1](https://img1.com)![img2](https://img2.com)", TextType.TEXT
+            )
+        ]
+        new_nodes = split_nodes_image(nodes)
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual(new_nodes[0].text, "img1")
+        self.assertEqual(new_nodes[0].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[0].url, "https://img1.com")
+        self.assertEqual(new_nodes[1].text, "img2")
+        self.assertEqual(new_nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[1].url, "https://img2.com")
+
+    def test_mixed_node_types(self):
+        nodes = [
+            TextNode("Code block", TextType.CODE),
+            TextNode("Normal text with ![alt](url)", TextType.TEXT),
+            TextNode("Link text", TextType.LINK, "url"),
+        ]
+        new_nodes = split_nodes_image(nodes)
+
+        self.assertEqual(new_nodes[0].text, "Code block")
+        self.assertEqual(new_nodes[0].text_type, TextType.CODE)
+
+        self.assertEqual(new_nodes[1].text, "Normal text with ")
+        self.assertEqual(new_nodes[1].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[2].text, "alt")
+        self.assertEqual(new_nodes[2].text_type, TextType.IMAGE)
+
+        self.assertEqual(new_nodes[3].text, "Link text")
+        self.assertEqual(new_nodes[3].text_type, TextType.LINK)
